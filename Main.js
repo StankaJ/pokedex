fda6d955e4359ed18a14a67e7b8063a893c1d81e
StankaJ <stanka.joseph@gmail.com>
2017-04-10 16:52:48 +0200

    asd

--- Main.js
+++ Main.js
@@ -1,5 +1,52 @@
 "use strict"
 
-function pokeCode() {
-    let a;
+var poke =
+[
+{
+  "id": 1,
+  "name": "Bulbasaur",
+  "type": [ "grass", "posion" ],
+  "sprite": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png"
+},
+{
+  "id": 5,
+  "name": "Charmeleon",
+  "type": [ "fire" ],
+  "sprite": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/5.png"
+},
+{
+  "id": 15,
+  "name": "Beedrill",
+  "type": [ "bug", "posion" ],
+  "sprite": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/15.png"
+},
+{
+  "id": 193,
+  "name": "Yanma",
+  "type": [ "bug", "flying" ],
+  "sprite": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/193.png"
+},
+{
+  "id": 152,
+  "name": "Chikorita",
+  "type": [ "grass" ],
+  "sprite": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/152.png"
+},
+]
+
+var root = document.querySelector(".pokemons");
+
+for (let it of poke)
+{
+  let pokemon = document.createElement("li");
+  let pokemonDl = document.createElement("dl");
+  let pokemonDt = document.createElement("dt");
+  let pokemonDd = document.createElement("dt");
+  let pokemonImage = document.createElement("img");
+  pokemonImage.src=it.sprite;
+  root.appendChild(pokemon);
+  pokemon.appendChild(pokemonDl);
+  pokemonDl.appendChild(pokemonDt);
+  pokemonDt.appendChild(pokemonImage);
+  pokemonDl.appendChild(pokemonDd);
 }
