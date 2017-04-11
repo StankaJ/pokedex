23a15d057bfd4f1e7d950e8fe7250880ccb141a0
László Lengyel <lacilengyel89@gmail.com>
2017-04-11 11:41:26 +0200

    loader added

--- Main.js
+++ Main.js
@@ -1,49 +1,51 @@
 "use strict"
 
 var poke =
-[
-{
-  "id": 1,
-  "name": "Bulbasaur",
-  "type": [ "grass", "posion" ],
-  "sprite": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png"
-},
-{
-  "id": 5,
-  "name": "Charmeleon",
-  "type": [ "fire" ],
-  "sprite": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/5.png"
-},
-{
-  "id": 15,
-  "name": "Beedrill",
-  "type": [ "bug", "posion" ],
-  "sprite": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/15.png"
-},
-{
-  "id": 193,
-  "name": "Yanma",
-  "type": [ "bug", "flying" ],
-  "sprite": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/193.png"
-},
-{
-  "id": 152,
-  "name": "Chikorita",
-  "type": [ "grass" ],
-  "sprite": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/152.png"
-},
-];
+  [
+    {
+      "id": 1,
+      "name": "Bulbasaur",
+      "type": ["grass", "posion"],
+      "sprite": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png"
+    },
+    {
+      "id": 5,
+      "name": "Charmeleon",
+      "type": ["fire"],
+      "sprite": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/5.png"
+    },
+    {
+      "id": 15,
+      "name": "Beedrill",
+      "type": ["bug", "posion"],
+      "sprite": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/15.png"
+    },
+    {
+      "id": 193,
+      "name": "Yanma",
+      "type": ["bug", "flying"],
+      "sprite": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/193.png"
+    },
+    {
+      "id": 152,
+      "name": "Chikorita",
+      "type": ["grass"],
+      "sprite": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/152.png"
+    },
+  ];
 
 var root = document.querySelector(".pokemons");
 
-setTimeout( function(){
-  for (let it of poke){
-    let pokemon = document.createElement("li"); 
+setTimeout(function () {
+  root.innerHTML = '';
+
+  for (let it of poke) {
+    let pokemon = document.createElement("li");
     let pokemonImage = document.createElement("img");
     let pokemonLabel = document.createElement("label");
-    pokemonImage.src=it.sprite;
+    pokemonImage.src = it.sprite;
     pokemonLabel.textContent = it.name;
-    pokemon.appendChild(pokemonImage);  
+    pokemon.appendChild(pokemonImage);
     pokemon.appendChild(pokemonLabel);
     root.appendChild(pokemon);
   }
