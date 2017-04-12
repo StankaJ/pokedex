720dc7183d26a8de3add193f034d17aa54b05c62
StankaJ <stanka.joseph@gmail.com>
2017-04-12 12:54:43 +0200

    modified dataProvider.js

--- dataProvider.js
+++ dataProvider.js
@@ -1,9 +1,30 @@
-var poke;
+var pokemons = [];
 
+fetchList(10);
+// "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other-sprites/official-artwork/" + id + ".png"
 
-fetch("https://pokeapi.co/api/v2/pokemon/?limit=200").then(function (response) {
-  return response.json().then(function (data) {
-    poke = data.results;
-    fillPoke('');
+function fetchList(limit) {
+  fetch("https://pokeapi.co/api/v2/pokemon/?limit=" + limit).then(function (response) {
+    return response.json().then(function (data) {
+      console.log(data);
+      for (i in data.results) {
+        let pokemon = {
+          id: "",
+          name,
+          url: "",
+        };
+        
+        pokemon.id = data.results[i].url.replace("https://pokeapi.co/api/v2/pokemon/", "").replace("/", "");
+        pokemon.name = data.results[i].name;
+        pokemon.url = data.results[i].url;
+
+        pokemons.push(pokemon);
+      }
+      console.log(pokemons);
+    });
   });
-});
+}
+
+function getPokemon(id) {
+  return pokemons.find("id", id);
+}
