b10d81fd1e39aa94214e2e331017169aa6b10f39
doda83 <gyori.jozsef@fornax.hu>
2017-04-12 12:02:38 +0200

    createPokemonLi

--- dataViewer.js
+++ dataViewer.js
@@ -4,30 +4,44 @@ function fillPoke(filter) {
     //@TODO kill me
     let root = document.querySelector(".pokemons");
     root.innerHTML = '';
-    console.log('Dosi' + filter);
 
     poke.forEach(function (pokemonItem) {
-      if (pokemonItem.name.startsWith(filter)) {
-        let pokemon = document.createElement("li");
-        let image = document.createElement("img");
-        let label = document.createElement("label");
         let id = pokemonItem.url.replace("https://pokeapi.co/api/v2/pokemon/", "").replace("/", "");
-        let pictureNumber = id;
-        image.src = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other-sprites/official-artwork/" + pictureNumber + ".png";
-        label.textContent = pokemonItem.name;
         pokemonItem.id=id;
-        pokemon.id='pokemon_'+id;
-        pokemon.addEventListener("click", function () { getDetails(id) });
-        pokemon.appendChild(image);
-        pokemon.appendChild(label);
-        pokemon.classList.add('pokemon');
-        root.appendChild(pokemon);
-      }
+        let pokemon = {};
+        pokemon.id = id;
+        pokemon.name=pokemonItem.name;
+        pokemon.src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other-sprites/official-artwork/" + id + ".png";
+        createPokemonLi(pokemon);
     })
     checkNoResult();
   }
   
 }
+
+function createPokemonLi(pokemon){
+    let pokemonListHolder = document.querySelector(".pokemons");
+    if (pokemonListHolder.querySelector(".pokemon_"+pokemon.id) == null)
+    {
+        let pokemonLi = document.createElement("li");
+        let image = document.createElement("img");
+        let label = document.createElement("label");
+        let pictureNumber = pokemon.id;
+        image.src = pokemon.src; //"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other-sprites/official-artwork/" + pictureNumber + ".png";
+        label.textContent = pokemon.name;
+        pokemonLi.id='pokemon_'+pokemon.id;
+        pokemonLi.addEventListener("click", function () { getDetails(id) });
+        pokemonLi.appendChild(image);
+        pokemonLi.appendChild(label);
+        pokemonLi.classList.add('pokemon');
+        pokemonListHolder.appendChild(pokemonLi);
+    }
+    else
+    {
+    
+    }
+}
+
 
 function filterPoke(filter)
 {
