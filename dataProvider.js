ec46e3e2dbce5179268fad31c5cebd3ee413fdd2
doda83 <gyori.jozsef@fornax.hu>
2017-04-12 14:36:15 +0200

    bugfix

--- dataProvider.js
+++ dataProvider.js
@@ -63,7 +63,7 @@ function getPokemon(id) {
   } else {
     pokemon = fetchPokemon(id);
   }
-
+}
 
 var pokemonTypes = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 10001, 10002];
 
